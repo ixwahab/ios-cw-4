cb7e1eec1aea98924326d60d8f7832ad34115bdd
Abdulwahab Alqallaf <nnoz0c@gmail.com>
2022-03-06 20:08:44 +0300

    done

---
+++ cw4.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var hobby = "football"
+
+let name = "Abdulwahab"
+
+var favouriteNumber = 4
+
+print("السلام عليكم، معاكم \(name), و رقمي المفضل هو \(favouriteNumber), و احب \(hobby)")
